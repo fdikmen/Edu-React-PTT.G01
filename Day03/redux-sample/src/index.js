dca635449e230108516a39c1549e603e5b899b84
FIRAT <frtdkmn@gmail.com>
2023-05-18 12:43:55 +0300

    Added Day04 Part01.

--- Day03/redux-sample/src/index.js
+++ Day03/redux-sample/src/index.js
@@ -3,18 +3,48 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux'
+import { combineReducers, createStore } from 'redux'
+
+
+//action
+/*
+const action = {type: "xxxx",payload:{count:12,name:"abc"}}
+in reducer
+function counterReducer(state,action)
+state.counter = action.payload.count
+*/
 
 function counterReducer() {
 return "hello"
 }
 
+function customReducer(state,action) {
+  //console.log("customReducer=>",state,action)
+  if(action.type === "INCREMENT") {
+    return action.payload.count
+  }
+  else if(action.type === "DECREMENT") {
+    return action.payload.count
+  }
+  return "Null"
+}
 
-let mystore = createStore(counterReducer)
+//const  allReducer = combineReducers({counterReducer,counterReducer2,  counterReducer3})
+//let mystore = createStore(allReducer)
 
-mystore.subscribe()
+// let mystore = createStore(counterReducer)
+let customStore = createStore(customReducer)
 
-mystore.dispatch({type: "INCREMENT",payload:123})
+console.log("INITIAL customStore States=>",customStore.getState())
+
+const customAction = {type: "INCREMENT",payload:{count:32,name:"abc"}}
+const customAction2 = {type: "DECREMENT",payload:{count:20,name:"abc"}}
+
+
+customStore.dispatch(customAction)
+console.log("customAction States=>",customStore.getState())
+customStore.dispatch(customAction2)
+console.log("customAction2 States=>",customStore.getState())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
